d136d07a70ba2f0cebe8abe2de3254a350376acd
nefir2 <vadim.ahtamov@gmail.com>
2023-05-12 10:07:16 +0300

    Добавьте файлы проекта.

---
+++ PR EU1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Input;
+
+namespace PR_EU1
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow() => InitializeComponent();
+
+		public void ColorChoose(object sender, RoutedEventArgs e)
+		{
+			if (sender is Button button) sender = new MenuItem() { Name = button.Content.ToString() };
+			mainwindow.Background = ((MenuItem)sender).Name switch
+			{
+				"red" => Brushes.Red,
+				"green" => Brushes.Green,
+				"blue" => Brushes.Blue,
+				"white" => Brushes.White,
+				"black" => Brushes.Black,
+				_ => Brushes.Purple,
+			};
+		}
+		private void AuthorClick(object sender, RoutedEventArgs e) => MessageBox.Show("top secret", "authors");
+		private void ExitClick(object sender, RoutedEventArgs e) => this.Close();
+		private void ShowStatus(object sender, MouseEventArgs e)
+		{
+			status.Text = sender.ToString();
+		}
+	}
+}
